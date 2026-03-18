24d995ca8b4c5fc9fd9db2b98519631b1a2ed0f0
kcnckgnnfjndglbdnf <anastasiakomova94@gmail.com>
2026-03-18 18:09:20 +0300

    справление пустых строк (удалены пробелы в конце файлов)

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def about(request):
-    """Страница "О проекте"."""
     return render(request, 'pages/about.html')
 
 
 def rules(request):
-    """Страница "Правила"."""
     return render(request, 'pages/rules.html')
