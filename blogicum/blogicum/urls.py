5380abc7005b7289e2bf33d7dece56fa5a840ea7
kcnckgnnfjndglbdnf <anastasiakomova94@gmail.com>
2026-04-01 20:22:50 +0300

    Добавлен Django Debug Toolbar

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
